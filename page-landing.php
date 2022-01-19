d227093daa70a709a30a50b0cf422a1f09f9cf61
Ekaterina <1988snea@gmail.com>
2022-01-19 22:09:55 +0500

    Вывод трех постов в лендинге

--- page-landing.php
+++ page-landing.php
@@ -8,13 +8,51 @@
 
 <main class="clearfix">
 	<div class="postsFlow clearfix">
-		<?php the_post() ?>
-		<article class="page-full">
-			<h2><?php the_title() ?></h2>
-			<div> <?php the_content() ?> </div>
+
+		<?php
+			$args = array( 'posts_per_page' => '3',
+			'post_status' => 'publish'
+			);
+
+			$query = new WP_Query( $args );
+			?>
+
+
+
+		<?php if ( $query->have_posts() ) : ?>
+		<?php while ( $query->have_posts() ) : $query->the_post();?>
+
+
+		<article class="post">
+			<div class="post-img post-img_position_left post-img_size_large"><img
+					src="<?php echo CFS()->get('img-large-left') ?>"></div>
+			<div class="post-img post-img_position_left post-img_size_small"><img
+					src="<?php echo CFS()->get('img-small-left') ?>"></div>
+			<div class="post-content">
+				<div class="post-text-wrap">
+					<div><?php the_title() ?></div>
+					<div> <?php echo CFS()->get('intro') ?> </div>
+				</div>
+				<div class="post-preview"><?php the_post_thumbnail('thumbnail') ?></div>
+
+			</div>
+			<div class="post-img post-img_position_right post-img_size_large"><img
+					src="<?php echo CFS()->get('img-large-right') ?>"></div>
+			<div class="post-img post-img_position_right post-img_size_small"><img
+					src="<?php echo CFS()->get('img-small-right') ?>"></div>
+
 		</article>
-	</div>
-	<div><?php dynamic_sidebar('sidebar-present') ?></div>
+
+		<?php endwhile;
+            wp_reset_postdata();
+            ?>
+		<?php else: ?>
+
+		<p>Записей нет.</p>
+
+		<?php endif; ?>
+
+		<div><?php dynamic_sidebar('sidebar-present') ?></div>
 
 </main>
 <?php get_footer() ?>

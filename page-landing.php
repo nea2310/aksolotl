603e0bac9a1bed7a25cf6ea2b3b711faeea606ab
Ekaterina <1988snea@gmail.com>
2022-02-06 23:23:09 +0500

    +

--- page-landing.php
+++ page-landing.php
@@ -7,11 +7,11 @@
 <?php get_header() ?>
 
 <main class="landing">
-	<?php dynamic_sidebar('sidebar-main-image') ?>
-	<?php dynamic_sidebar('sidebar-infoblock-top') ?>
-	<section class="posts">
+  <?php dynamic_sidebar('sidebar-main-image') ?>
+  <?php dynamic_sidebar('sidebar-infoblock-top') ?>
+  <section class="posts">
 
-		<?php
+    <?php
 			$args = array( 'posts_per_page' => '3',
 			'post_status' => 'publish',
 			'category_name' => 'landing'
@@ -22,13 +22,15 @@
 
 
 
-		<?php if ( $query->have_posts() ) : ?>
-		<?php while ( $query->have_posts() ) : $query->the_post();
+    <?php if ( $query->have_posts() ) : ?>
+    <?php while ( $query->have_posts() ) : $query->the_post();
 		
 		$imgLargeLeft= CFS()->get('img-large-left');
 		$imgSmallLeft= CFS()->get('img-small-left');
 		$imgLargeRight= CFS()->get('img-large-right');
 		$imgSmallRight= CFS()->get('img-small-right');
+    $imgPreview= CFS()->get('img-preview');
+    $imgIcon= CFS()->get('post-icon');
 
 		$imgLargeLeftEmblem= CFS()->get('img-large-left-emblem');
 		$imgSmallLeftEmblem= CFS()->get('img-small-left-emblem');
@@ -38,100 +40,101 @@
 
 
 
-		<article class="post">
+    <article class="post">
 
 
 
 
 
-			<?php if ( $imgLargeLeft): ?>
-			<div class="post-img post-img_position_left post-img_size_large">
+      <?php if ( $imgLargeLeft): ?>
+      <div class="post-img post-img_position_left post-img_size_large">
 
-				<div class="post-img-picture">
-					<img src="<?php echo $imgLargeLeft ?>">
-				</div>
+        <div class="post-img-picture">
+          <img src="<?php echo $imgLargeLeft ?>">
+        </div>
 
-				<?php if ( $imgLargeLeftEmblem): ?>
-				<div class="post-img-emblem">
-					<img src="<?php echo $imgLargeLeftEmblem ?>">
-				</div>
-				<?php endif ?>
+        <?php if ( $imgLargeLeftEmblem): ?>
+        <div class="post-img-emblem">
+          <img src="<?php echo $imgLargeLeftEmblem ?>">
+        </div>
+        <?php endif ?>
 
-			</div>
-			<?php endif ?>
-
-
-
-
-			<?php if ( $imgSmallLeft): ?>
-			<div class="post-img post-img_position_left post-img_size_small"><img src="<?php echo $imgSmallLeft ?>">
-
-				<?php if ( $imgSmallLeftEmblem): ?>
-				<div class="post-img-emblem"><img src="<?php echo $imgSmallLeftEmblem ?>">
-				</div>
-				<?php endif ?>
-
-			</div>
-			<?php endif ?>
+      </div>
+      <?php endif ?>
 
 
 
 
+      <?php if ( $imgSmallLeft): ?>
+      <div class="post-img post-img_position_left post-img_size_small"><img src="<?php echo $imgSmallLeft ?>">
 
-			<div class="post-content">
-				<div class="post-text-wrap">
-					<div><?php the_title() ?></div>
-					<div> <?php echo CFS()->get('intro') ?> </div>
-				</div>
-				<div class="post-preview"><?php the_post_thumbnail('thumbnail') ?></div>
-			</div>
+        <?php if ( $imgSmallLeftEmblem): ?>
+        <div class="post-img-emblem"><img src="<?php echo $imgSmallLeftEmblem ?>">
+        </div>
+        <?php endif ?>
 
-
-
-			<?php if ( $imgLargeRight): ?>
-			<div class="post-img post-img_position_right post-img_size_large"><img src="<?php echo $imgLargeRight ?>">
-
-				<?php if ( $imgLargeRightEmblem): ?>
-				<div class="post-img-emblem"><img src="<?php echo $imgLargeRightEmblem ?>">
-				</div>
-				<?php endif ?>
-
-			</div>
-			<?php endif ?>
+      </div>
+      <?php endif ?>
 
 
 
 
-			<?php if ( $imgSmallRight): ?>
-			<div class="post-img post-img_position_right post-img_size_small"><img src="<?php echo $imgSmallRight?>">
 
-				<?php if ( $imgSmallRightEmblem): ?>
-				<div class="post-img-emblem"><img src="<?php echo $imgSmallRightEmblem ?>">
-				</div>
-				<?php endif ?>
+      <div class="post-content">
+        <div class="post-text-wrap">
+          <div class="post-icon"><img src="<?php echo $imgIcon ?>"></div>
+          <div class="post-title"><?php the_title() ?></div>
+          <div class="post-intro"> <?php echo CFS()->get('intro') ?> </div>
+        </div>
+        <div class="post-preview"><img src="<?php echo $imgPreview ?>"></div>
+      </div>
 
-			</div>
-			<?php endif ?>
 
 
-		</article>
+      <?php if ( $imgLargeRight): ?>
+      <div class="post-img post-img_position_right post-img_size_large"><img src="<?php echo $imgLargeRight ?>">
 
-		<?php endwhile;
+        <?php if ( $imgLargeRightEmblem): ?>
+        <div class="post-img-emblem"><img src="<?php echo $imgLargeRightEmblem ?>">
+        </div>
+        <?php endif ?>
+
+      </div>
+      <?php endif ?>
+
+
+
+
+      <?php if ( $imgSmallRight): ?>
+      <div class="post-img post-img_position_right post-img_size_small"><img src="<?php echo $imgSmallRight?>">
+
+        <?php if ( $imgSmallRightEmblem): ?>
+        <div class="post-img-emblem"><img src="<?php echo $imgSmallRightEmblem ?>">
+        </div>
+        <?php endif ?>
+
+      </div>
+      <?php endif ?>
+
+
+    </article>
+
+    <?php endwhile;
             wp_reset_postdata();
             ?>
-		<?php else: ?>
+    <?php else: ?>
 
-		<p>Записей нет.</p>
+    <p>Записей нет.</p>
 
-		<?php endif; ?>
-	</section>
-
-
+    <?php endif; ?>
+  </section>
 
 
 
-	<section class="slider">
-		<?php
+
+
+  <section class="slider">
+    <?php
 			$args1 = array( 'posts_per_page' => '3',
 			'post_status' => 'publish',
 			'terms'    => ['post-format-aside'],
@@ -146,24 +149,24 @@
 				]
 		  ]);
 			?>
-		<?php if ( $query1->have_posts() ) : ?>
-		<?php while ( $query1->have_posts() ) : $query1->the_post();
+    <?php if ( $query1->have_posts() ) : ?>
+    <?php while ( $query1->have_posts() ) : $query1->the_post();
 		?>
-		<article class="post">
-			<div class="post-content">
-				<div class="post-text-wrap">
-					<div><?php the_title() ?></div>
-					<div> <?php echo CFS()->get('intro') ?> </div>
-				</div>
-			</div>
-		</article>
-		<?php endwhile;
+    <article class="post">
+      <div class="post-content">
+        <div class="post-text-wrap">
+          <div><?php the_title() ?></div>
+          <div> <?php echo CFS()->get('intro') ?> </div>
+        </div>
+      </div>
+    </article>
+    <?php endwhile;
             wp_reset_postdata();
             ?>
-		<?php else: ?>
-		<p>Записей нет.</p>
-		<?php endif; ?>
-	</section>
+    <?php else: ?>
+    <p>Записей нет.</p>
+    <?php endif; ?>
+  </section>
 
 
 
